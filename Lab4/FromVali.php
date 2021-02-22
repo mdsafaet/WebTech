d1241d3b632b3e47d2752d8e2e983611b883f129
Safaet <safaethossainsafu@gmail.com>
2021-02-22 14:14:30 +0600

    lab4 half

---
+++ Lab4/FromVali.php
@@ -0,0 +1,172 @@
+<?php
+	$name="";
+	$err_name="";
+	$uname="";
+	$err_uname="";
+	$pass="";
+	$err_pass="";
+	$cpass="";
+	$err_cpass="";
+	$email="";
+	$err_email="";
+	$code="";
+	$err_code="";
+	$num="";
+	$err_num="";
+	$bio="";
+	$err_bio="";
+	
+	
+		if	($_SERVER["REQUEST_METHOD"]=="POST")
+		{
+			if (empty($_POST["name"]))
+			{
+				$err_name="**Name Required";
+			}
+			else
+			{
+				$name=$_POST["name"];
+			}
+			if (empty($_POST["uname"]))
+			{
+				$err_uname="**Username Required";
+			}
+			else if(strlen($_POST["uname"])<8)
+			{
+				$err_uname="**Username must be 8 charachters long";
+			}
+			else
+			{
+				$uname=$_POST["uname"];
+			}
+			if (empty($_POST["pass"]))
+			{
+				$err_pass="**Password Required";
+			}
+			 elseif(strpos($_POST["pass"],":"))
+			{
+				$err_pass="**Password should not contain ':'";
+			}
+			else
+			{
+				$pass=$_POST["pass"];
+			}
+			if (empty($_POST["bio"]))
+			{
+				$err_bio="**Bio can not be blank";
+			}
+			else
+			{
+				$bio=$_POST["bio"];
+			}
+			
+		
+		
+?>
+<html>
+	<head></head>
+	<body>
+		<center>
+		<form action="" method="post">
+		<fieldset>
+		<legend align="center">Club Member Registration</legend>
+			<table>
+				<tr>
+					<td><span >Name</span></td>
+					<td>:</td>
+					<td><input type="text" name="name" value="<?php echo $name;?>"> </td><td><span><?php echo $err_name;?></span></td>
+				</tr>
+				
+				<tr>
+					<td><span >Username</span></td>
+					<td>:</td>
+					<td><input type="text" name="uname" value="<?php echo $uname;?>"> </td><td><span><?php echo $err_uname;?></span></td>
+				</tr>
+				<tr>
+					<td><span>Password</span></td>
+					<td>:</td>
+					<td><input type="password" name="pass" value="<?php echo $pass;?>"> </td><td><span><?php echo $err_pass;?></span></td>
+				</tr>
+				<tr>
+					<td><span>Confirm Password</span></td>
+					<td>:</td>
+					<td><input type="password" name="cpass" value="<?php echo $cpass;?>"> </td><td><span><?php echo $err_cpass;?></span></td>
+				</tr>
+				<tr>
+					<td><span>Email</span></td>
+					<td>:</td>
+					<td><input type="text" name="email" value="<?php echo $email;?>"> </td><td><span><?php echo $err_email;?></span></td>
+				</tr>
+				<tr>
+					<td><span>Phone</span></td>
+					<td>:</td>
+					<td><input type="text" name="code" value="<?php echo $code;?>" placeholder="Code" size="4">-<input type="text" name="num" value="<?php echo $num;?>" placeholder = "Number" size="10"> </td><td><span><?php echo $err_code;?></span><span><?php echo $err_num;?></span></td>
+				</tr>
+				<tr>
+					<td><span>Address</span></td>
+					<td>:</td>
+					<td><input type="text" name="sa" placeholder="Street Address"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td><input type="text" name="city" placeholder = "City" size="6">-<input type="text" name="state" placeholder = "State" size="8"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><td>
+					<input type="text" name="Pocode" placeholder = "Postal/Zip Code">
+				</tr>
+				
+				<tr>
+				<td><span>Birth Date</span></td>
+				<td>:</td>
+				<td>
+				<select name="day">
+					<option disabled selected>Day</option>
+					<?php
+						for($i=1;$i<=31;$i++)
+						{
+							echo "<option>$i</option>";
+						}
+					?>
+					</select>
+					<select name="month">
+					<option disabled selected>Month</option>
+					<?php
+						$mon= array("January","February","March","April","May","June","July","August","September","October","November","December");
+						for($j=0;$j<count($mon);$j++)
+						{
+							echo "<option>$mon[$j]</option>";
+						}
+					?>
+				</select>
+				<select name="year">
+					<option disabled selected>Year</option>
+					<?php
+						for($k=1971;$k<=2040;$k++)
+						{
+							echo "<option>$k</option>";
+						}
+					?>
+				</select>
+				</td>
+				</tr>
+				
+				
+				<tr>
+				<td><span>Bio</span></td>
+				<td>:</td>
+				<td><textarea name="bio" ></textarea></td>
+				<td><span><?php echo $err_bio;?></span></td>
+				</tr>
+				<tr>
+				<td>
+				<input type="submit" name="submit" value="Submit">
+				</td>
+				</tr>
+			</table>
+		</form>
+		</center>
+	</body>
+</html>
